e2eed88d48ed6c21a75f01204e5a5c1ef5e26492
Pan Yue <zxc479773533@gmail.com>
2018-11-06 23:01:27 +0800

    Add lab01 libs

---
+++ lab01/stack.cpp
@@ -0,0 +1,130 @@
+/*
+ * HUST-CXX-Labs lab01::implementation
+ * 
+ * Created by zxcpyp
+ * 
+ * Github: zxc479773533
+ */
+
+#include "stack.h"
+
+/*
+ * initSTACK - Initial the stack with m elem at most
+ */
+void initSTACK(STACK *const p, int m) {
+	p->elems = (int*)malloc(sizeof(int) * m);
+	if (p != NULL) {
+		p->max = m;
+		p->pos = 0;
+	}
+}
+
+
+/*
+ * initSTACK - Initial the stack using another stack
+ */
+void initSTACK(STACK *const p, const STACK &s) {
+	p->elems = (int*)malloc(sizeof(int) * s.max);
+	if (p != NULL) {
+		memcpy(p->elems, s.elems, s.max);
+		p->max = s.max;
+		p->pos = s.pos;
+	}
+}
+
+/*
+ * size - Get the max elem num of stack p
+ */
+int size(const STACK *const p) {
+	if (p == NULL)
+		return 0;
+	return p->max;
+}
+
+/*
+ * howMany - Get the elem num in stack p
+ */
+int howMany(const STACK *const p) {
+	if (p == NULL)
+		return 0;
+	return p->pos;
+}
+
+/*
+ * getelem - Get the elem at pos x in stack p
+ */
+int getelem(const STACK *const p, int x) {
+	if (p == NULL || x >= p->pos)
+		return 0;
+	return p->elems[x];
+}
+
+/*
+ * push - Push e into stack p
+ */
+STACK *const push(STACK *const p, int e) {
+	if (p == NULL)
+		return NULL;
+	// Stack full
+	if (p->pos == p->max)
+		return p;
+	// Move to right
+	for (int i = p->pos; i > 0; i--)
+		p->elems[i] = p->elems[i - 1];
+	// New data
+	p->elems[0] = e;
+	p->pos++;
+	return p;
+}
+
+/*
+ * pop - Pop an elem out of stack p
+ */
+STACK *const pop(STACK *const p, int &e) {
+	if (p == NULL)
+		return NULL;
+	// Stack empty
+	if (p->pos == 0)
+		return p;
+	// New data
+	e = p->elems[0];
+	// Move to left
+	for (int i = 0; i < p->pos - 1; i++)
+		p->elems[i] = p->elems[i + 1];
+	p->pos--;
+	return p;
+}
+
+/*
+ * assign - Assign data from s to p
+ */
+STACK *const assign(STACK *const p, const STACK &s) {
+  if (p != NULL) {
+    memcpy(p->elems, s.elems, s.max);
+    p->max = s.max;
+    p->pos = s.pos;
+  }
+  return p;
+}
+
+/*
+ * print - Print data of p
+ */
+void print(const STACK *const p) {
+	if (p != NULL && p->pos > 0) {
+		for (int i = 0; i < p->pos - 1; i++) {
+			printf("%d ", p->elems[i]);
+		}
+		printf("%d", p->elems[p->pos - 1]);
+	}
+}
+
+/*
+ * destroySTACK - Destroy the stack
+ */
+void destroySTACK(STACK *const p) {
+	if (p != NULL) {
+		free(p->elems);
+		free(p);
+	}
+}
---
+++ lab01/stack.h
@@ -0,0 +1,110 @@
+/*
+ * HUST-CXX-Labs lab01::headfile
+ * 
+ * Created by zxcpyp
+ * 
+ * Github: zxc479773533
+ */
+
+#ifndef HUST_CXX_LAB01
+#define HUST_CXX_LAB01
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct STACK {
+  int *elems; // Store the elem
+  int max;    // Max elem nums
+  int pos;    // Elem nums
+} STACK;
+
+// Operations on stack
+
+/*
+ * initSTACK - Initial the stack with m elem at most
+ * parameters：
+ *  p: stack pointer
+ *  m: max elem nums
+ * return: void
+ */
+void initSTACK(STACK *const p, int m);
+
+/*
+ * initSTACK - Initial the stack using another stack
+ * parameters：
+ *  p: stack pointer
+ *  m: another stack pointer
+ * return: void
+ */
+void initSTACK(STACK *const p, const STACK &s);
+
+/*
+ * size - Get the max elem num of stack p
+ * parameters：
+ *  p: stack pointer
+ * return: the max elem num of p
+ */
+int size(const STACK *const p);
+
+/*
+ * howMany - Get the elem num in stack p
+ * parameters：
+ *  p: stack pointer
+ * return: the elem num in p
+ */
+int howMany(const STACK *const p);
+
+/*
+ * getelem - Get the elem at pos x in stack p
+ * parameters：
+ *  p: stack pointer
+ *  x: pos
+ * return: the elem at pos x
+ */
+int getelem(const STACK *const p, int x);
+
+/*
+ * push - Push e into stack p
+ * parameters：
+ *  p: stack pointer
+ *  e: new elem
+ * return: the stack pointer
+ */
+STACK *const push(STACK *const p, int e);
+
+/*
+ * pop - Pop an elem out of stack p
+ * parameters：
+ *  p: stack pointer
+ *  &e: new elem
+ * return: the stack pointer
+ */
+STACK *const pop(STACK *const p, int &e);
+
+/*
+ * assign - Assign data from s to p
+ * parameters：
+ *  p: stack pointer
+ *  &s: data
+ * return: the stack pointer
+ */
+STACK *const assign(STACK *const p, const STACK &s);
+
+/*
+ * print - Print data of p
+ * parameters：
+ *  p: stack pointer
+ * return: void
+ */
+void print(const STACK *const p);
+
+/*
+ * destroySTACK - Destroy the stack
+ * parameters：
+ *  p: stack pointer
+ * return: void
+ */
+void destroySTACK(STACK *const p);
+
+#endif // !HUST_CXX_LAB01

e8d720cb41a7c3b1cd72081d1c402623249f2f57
Pan Yue <zxc479773533@gmail.com>
2018-11-07 22:09:34 +0800

    Finish lab02

---
+++ lab02/stack.h
@@ -0,0 +1,33 @@
+/*
+ * HUST-CXX-Labs lab02::headfile
+ * 
+ * Created by zxcpyp
+ * 
+ * Github: zxc479773533
+ */
+
+#ifndef HUST_CXX_LAB02
+#define HUST_CXX_LAB02
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+class STACK {
+  int *const elems; // Store the elem
+  const int max;    // Max elem nums
+  int pos;          // Elem nums
+public:
+  STACK(int m);                  // Initial the stack with m elem at most
+  STACK(const STACK &s);         // Initial the stack using another stack
+  int size() const;              // Get the elem num in stack p
+  int howMany() const;           // Get the elem num in stack p
+  int getelem(int x) const;      // Get the elem at pos x in stack p
+  STACK &push(int e);            // Push e into stack p
+  STACK &pop(int &e);            // Pop an elem out of stack p
+  STACK &assign(const STACK &s); // Assign data from s to p
+  void print() const;            // Print data of p
+  ~STACK();                      // Destroy the stack
+};
+
+#endif // !HUST_CXX_LAB02

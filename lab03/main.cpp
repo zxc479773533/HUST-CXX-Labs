458f7a07aad01f568c70001d97df24be6897690c
Pan Yue <zxc479773533@gmail.com>
2018-11-10 16:36:42 +0800

    Finish lab03

---
+++ lab03/main.cpp
@@ -0,0 +1,99 @@
+/*
+ * HUST-CXX-Labs lab02::main
+ * 
+ * Created by zxcpyp
+ * 
+ * Github: zxc479773533
+ */
+
+#include <iostream>
+#include "stack.h"
+#include "cmd_parser.h"
+
+using namespace std;
+
+int main(int argc, char **argv) {
+  char op;
+  int arg;
+  STACK *mystack = nullptr;
+  // Set output file
+  cmd_paresr lab01_parser = cmd_paresr(argc, argv);
+  while (lab01_parser.now_pos() < lab01_parser.ops_num()) {
+    queue<int> arg_list;
+    op = lab01_parser.next_step(arg_list);
+    switch (op) {
+      case 'S': {
+        cout << "S";
+        arg = arg_list.front();
+        mystack = new STACK(arg);
+        cout << " " << arg;
+        arg_list.pop();
+        break;
+      }
+      case 'I': {
+        cout << " I";
+        int size = arg_list.size();
+        // Test if the stack can overflow
+        if (mystack->size() - int(*mystack) < size) {
+          cout << " E" << endl;
+          exit(0);
+        }
+        for (int i = 0; i < size; i++) {
+          arg = arg_list.front();
+          *mystack << arg;
+          arg_list.pop();
+        }
+        cout << " ";
+        mystack->print();
+        break;
+      }
+      case 'O': {
+        cout << " O";
+        // Test if the stack can be empty
+        if (int(*mystack) < arg_list.size()) {
+          cout << " E" << endl;
+          exit(0);
+        }
+        int e;
+        arg = arg_list.front();
+        for (int i = 0; i < arg; i++)
+          *mystack >> e;
+        arg_list.pop();
+        cout << " ";
+        mystack->print();
+        break;
+      }
+      case 'A': {
+        cout << " A";
+        arg = arg_list.front();
+        STACK *new_stack = new STACK(arg);
+        *new_stack = *mystack;
+        mystack = new_stack;
+        arg_list.pop();
+        cout << " ";
+        mystack->print();
+        break;
+      }
+      case 'C': {
+        cout << " C";
+        STACK *new_stack = new STACK(*mystack);
+        mystack = new_stack;
+        break;
+      }
+      case 'N': {
+        cout << " N";
+        cout << " " << int(*mystack);        
+        break;
+      }
+      case 'G': {
+        cout << " G";
+        arg = arg_list.front();
+        cout << " " << mystack[arg];
+        arg_list.pop();
+        break;
+      }
+    }
+  }
+  cout << endl;
+  return 0;
+}
